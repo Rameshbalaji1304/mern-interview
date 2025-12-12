f27f522cd9438cf930cec031c43cb3a89345c2ad
rameshbalaji <ramesha7h@gmail.com>
2025-12-12 12:35:07 +0530

    created a auth and db setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,16 @@ import express from 'express'
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
 import path from 'path'
+import cors from 'cors'
+import {serve} from 'inngest/express'
+import { functions,inngest } from './lib/inngest.js';
+
 
 const app=express();
+app.use(express.json())
+//credentials=> server allows a browser to include cookies in req
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 const __dirname=path.resolve();
 
