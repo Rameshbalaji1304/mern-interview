4506c54cb24b4c162275a8513fc1cb4431a9f83f
rameshbalaji <ramesha7h@gmail.com>
2025-12-16 01:53:04 +0530

    auth and stream sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,30 @@
+import {StreamChat} from 'stream-chat'
+import { ENV } from './env.js'
+
+
+const apiKey=ENV.STREAM_API_KEY;
+const apiSecret=ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret){
+    console.error("Missing API KEY or API SERET KEY");
+}
+
+export default chatClient=StreamChat.getInstance(apiKey,apiSecret);
+
+export const upsertStreamUser=async(userData)=>{
+    try {
+        await chatClient.upsertUser(userData)
+      console.log("Stream User upserted Success!",userData)
+    } catch (error) {
+        console.error("Error upserting Stream User:",error);
+    }
+}
+
+export const deleteStreamUser=async(userId)=>{
+    try {
+        await chatClient.deleteUser([userId])
+        console.log("Stream User deleted Success!",userId)
+    } catch (error) {
+        console.error("Error deleting Stream User:",error);
+    }
+}
